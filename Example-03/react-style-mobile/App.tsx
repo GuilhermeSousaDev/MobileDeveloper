40481d8328a0645c2153b0f8173d3b71db0a3efc
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-07-27 09:12:59 -0300

    Flex React Native

--- Example-03/react-style-mobile/App.tsx
+++ Example-03/react-style-mobile/App.tsx
@@ -1,9 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.box}></View>
+      <View style={[styles.box, styles.children]}></View>
+      <View style={styles.box}></View>
+      <View style={styles.box}></View>
+      <View style={styles.box}></View>
+      <View style={styles.box}></View>
       <View style={styles.box}></View>
       <View style={styles.box}></View>
       <View style={styles.box}></View>
@@ -17,12 +23,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'space-between',
+    flexDirection: 'row',
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignContent: 'space-around',
+    flexWrap: 'wrap',
+    paddingTop: 50,
+    paddingBottom: 50
   },
   box: {
     width: 100,
     height: 100,
-    backgroundColor: 'red'
+    backgroundColor: 'red',
+    borderRadius: 15
+  },
+  children: {
+    alignSelf: 'center',
+    backgroundColor: 'blue'
   }
 });
