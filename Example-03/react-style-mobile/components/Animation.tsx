3c506ca6d2722d6bde38e7803b67b8c059a2108b
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-08-04 10:01:30 -0300

    interpolate values in Animated RN

--- Example-03/react-style-mobile/components/Animation.tsx
+++ Example-03/react-style-mobile/components/Animation.tsx
@@ -4,12 +4,13 @@ import { Animated, Easing, StyleSheet, Text } from "react-native";
 export default function Animation() {
     const [opacityAnim] = useState(new Animated.Value(0));
     const [scaleAnim] = useState(new Animated.Value(0));
-    
+    const [colorAnim] = useState(new Animated.Value(0));
+
     useEffect(() => {
         Animated.sequence([
             Animated.timing(opacityAnim, {
                 toValue: 1,
-                duration: 10000,
+                duration: 1000,
                 useNativeDriver: true
             }),
             Animated.timing(scaleAnim, {
@@ -18,18 +19,37 @@ export default function Animation() {
                 easing: Easing.bounce,
                 useNativeDriver: true
             }),
+            Animated.timing(colorAnim, {
+                toValue: 1,
+                duration: 3000,
+                easing: Easing.in,
+                useNativeDriver: false
+            })
         ]).start();
     }, [opacityAnim, scaleAnim]);
 
     return (
         <Animated.View style={[ styles.container, { opacity: opacityAnim } ]}>
-            <Text style={styles.box}>Box</Text>
+            <Text style={styles.box}>Box 1</Text>
 
             <Animated.View style={[ styles.box, { transform: [{ scale: scaleAnim }] }]}>
                 <Text>Box 2</Text>
             </Animated.View>
 
-            
+            <Animated.View 
+                style={[ 
+                    styles.box, 
+                    { backgroundColor: colorAnim.interpolate({ 
+                        inputRange: [0, 1],
+                        outputRange: ['blue', 'black']
+                    })}
+                ]}
+            >
+                <Animated.Text style={{ color: colorAnim.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: ['black', 'white']
+                }) }}>Box 3</Animated.Text>
+            </Animated.View>
         </Animated.View>
     )
 }
