2e88f7557e4eda24cd9168fdbe83fd39c0b05751
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-08-12 09:31:28 -0300

    Gesture System

--- example-04/rn-examples/App.tsx
+++ example-04/rn-examples/App.tsx
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react';
+import { View, StyleSheet, Text, Alert } from 'react-native';
+ 
 export default function App() {
+ 
+  const showAlert = () => {
+    Alert.alert('View Clicked ...');
+  };
+ 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+ 
+    <View style={styleSheet.MainContainer}>
+ 
+      <View style={styleSheet.view}
+        onStartShouldSetResponder={() => true}
+        onResponderGrant={showAlert}
+      >
+ 
+        <Text style={{ fontSize: 28, color: 'white', textAlign: 'center' }}> 
+          Set onPress onClick on View Component in React Native
+        </Text>
+ 
+      </View>
+ 
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
+ 
+const styleSheet = StyleSheet.create({
+ 
+  MainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+ 
+  view: {
+    width: 350,
+    height: 120,
+    backgroundColor: 'coral',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
