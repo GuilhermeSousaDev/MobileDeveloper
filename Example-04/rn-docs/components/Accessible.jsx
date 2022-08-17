65730928b9c4d235921fa3350bbd84ef2ed52fb3
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-08-17 08:58:08 -0300

    accessibilityLiveRegion assertive prop

--- Example-04/rn-docs/components/Accessible.jsx
+++ Example-04/rn-docs/components/Accessible.jsx
@@ -1,10 +1,26 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Text, Alert } from 'react-native';
 
 export default function Accessible() {
+    const [count, setCount] = useState<number>(0);
+
     return (
-        <View style={styles.container} accessible={true}>
+        <View 
+            style={styles.container} 
+            onPress={() => Alert.alert('Alert...')}
+            accessible={true} 
+            accessibilityLabel='Tap me'
+            accessibilityHint='Show Alert'
+            accessibilityLanguage='pt-PT'
+        >
             <Text>Text 1</Text>
             <Text>Text 2</Text>
+
+            <Button onPress={() => setCount(count + 1)} />
+
+            <View accessibilityLiveRegion='assertive'>
+                <Text>{ count }</Text>
+            </View>
         </View>
     )
 }
